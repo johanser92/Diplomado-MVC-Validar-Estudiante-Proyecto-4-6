3448b88450e8f3fdaa96eac6bfe6fbcc884d88b9
johanser92 <40702293+johanser92@users.noreply.github.com>
2019-12-02 23:12:06 -0400

    Johanser92
    Proyecto 4-6

---
+++ Diplomado-MVC-Validar-Estudiante/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Diplomado_MVC_Validar_Estudiante.MvcApplication" Language="C#" %>
---
+++ Diplomado-MVC-Validar-Estudiante/Models/Estudiante.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Diplomado_MVC_Validar_Estudiante.Models
+{
+    public class Estudiante
+    {   [Required(ErrorMessage = "El Nombre es Obligatorio")]
+        [MinLength(3,ErrorMessage ="El nombre del estudiante debe tener al menos 3 caracteres")]
+        public String Nombres { get; set; }
+
+        [Required(ErrorMessage = "El Apellido es Obligatorio")]
+        [MinLength(2, ErrorMessage = "El Apellido del estudiante debe tener al menos 2 caracteres")]
+        public String Apellidos { get; set; }
+
+        [Range(14,65, ErrorMessage ="La edad del Estudiante debe de estar entre 14 y 65")]
+        public int Edad { get; set; }
+
+        [EmailAddress(ErrorMessage ="Debe ingrsar un mail valido")]
+        public String Email { get; set; }
+
+        [RegularExpression("[MmFf]", ErrorMessage ="Solo se puede ingresar una M=Masculino o F=Femenino")]
+        public String Genero { get; set; }
+
+        [RegularExpression("[CcSsVvDd]", ErrorMessage ="Solo puede ingresar una C=Casado, S=Soltero, U=Union Libre o D=Divociado")]
+        public String EstadoCivil { get; set; }
+    }
+}
